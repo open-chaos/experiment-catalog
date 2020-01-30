e390e0a5a63f674a78ada9c3ad3a7f09f6533086
Russ Miles <russ@russmiles.com>
2020-01-30 16:06:00 +0000

    Corrected scripts and parameters for db experiment
    Contributes to #110
    
    Signed-off-by: Russ Miles <russ@russmiles.com>

--- db/sqlserver/kill_server_secondary_failover/listener_is_online.ps1
+++ db/sqlserver/kill_server_secondary_failover/listener_is_online.ps1
@@ -1,19 +1,12 @@
 param(
         [string]$Server,
-        [string]$AvailabilityGroup
+        [string]$ListenerName,
+        [int]$Port
     )
     $ErrorActionPreference = "Stop"
 try{  
-    # import sqlserver module
-    import-module sqlserver    
-    # retrieving the availability group listener name
-    $SqlGetListener = "SELECT dns_name, port
-    FROM sys.availability_group_listeners AS l
-    INNER JOIN sys.availability_groups AS a ON l.group_id = a.group_id
-    WHERE a.name = '$AvailabilityGroup'"
-    $Listener = Invoke-SqlCmd -ServerInstance $Server -Database master -Query $SqlGetListener
-    # testing connection to listener
-    $Connected = Test-NetConnection -ComputerName $Listener.dns_name -Port $Listener.port
+        # testing connection to listener
+    $Connected = Test-NetConnection -ComputerName $ListenerName -Port $Port
     # if TCP test returns true...return 0
     if($Connected.TcpTestSucceeded -eq $true){
         return 0
